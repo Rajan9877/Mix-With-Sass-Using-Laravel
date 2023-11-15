9ce247bdca6514954327acf5069622fe810e757e
Rajan Joriya <rajanjoriya000000@gmail.com>
2023-11-15 16:31:18 +0530

    My First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>Sass Practice</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Contact Us</a></li>
+        </ul>
+    </nav>
+    <main>
+        <h1>Welcome To This Website</h1>
+        <p>
+            Certainly! However, I'll need more information about the website's purpose, content, or topic to provide you with a relevant paragraph. Could you please specify the type of website or provide some key points you'd like to include?
+        </p>
+    </main>
+    <footer>
+        <p>Copyright &copy; 2023 | Created By Rajan Joriya</p>
+    </footer>
+</body>
+</html>
